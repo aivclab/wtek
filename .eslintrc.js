99eccf3cba368077e76dd2e1f271f1bb0c115ff7
Christian Heider Nielsen <christian.heider@alexandra.dk>
2021-11-04 13:33:00 +0100

    Address Thomas comments

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,15 +3,12 @@ module.exports = {
     browser: true,
     es2021: true
   },
-  extends: ['plugin:react/recommended', 'standard'],
+  extends: ['eslint:recommended', 'standard'],
   parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    },
     ecmaVersion: 11,
     sourceType: 'module'
   },
-  plugins: ['react'],
+
   globals: {
     window: true,
     module: true,
