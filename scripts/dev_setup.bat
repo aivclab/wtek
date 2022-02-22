5457c51ce2b5547974d9d13e1bcaf1df2d6878f5
Christian Heider Nielsen <christian.heider@alexandra.dk>
2022-02-22 20:27:01 +0100

    add dependabot.yml

--- scripts/dev_setup.bat
+++ scripts/dev_setup.bat
@@ -1,5 +1,15 @@
 cd ..\wtek || exit /b
 rd -r ".\node_modules\"
-npm install
-npm link wfundament
-npm link
+del package-lock.json
+
+
+cd ..\wfundament || exit /b
+rd -r ".\node_modules\"
+del package-lock.json
+call npm install
+call npm link
+
+cd ..\wtek || exit /b
+call npm link wfundament
+call npm install
+call npm link
--- scripts/dev_setup.sh
+++ scripts/dev_setup.sh
@@ -1,5 +1,12 @@
 cd ../wtek || exit
-rm -r node_modules
+rm -r node_modules package-lock.json
+
+cd ../wfundament || exit
+rm -r node_modules package-lock.json
 npm install
+npm link
+
+cd ../wtek || exit
 npm link wfundament
+npm install
 npm link
